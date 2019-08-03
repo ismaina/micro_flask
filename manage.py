65d6c09b672ba70afa70a9dbcc0a3ab1ae760153
ismaina <maina.wanjau@gmail.com>
2019-08-03 13:52:10 +0300

    initial testing configs

--- manage.py
+++ manage.py
@@ -1,11 +1,21 @@
+import sys
+import unittest
 from flask.cli import  FlaskGroup
 from project import app, db
 
 cli = FlaskGroup(app)
 
+@cli.command()
+def test():
+     """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
+
 
 @cli.command('recreate_db')
-
 def recreate_db():
     db.drop_all()
     db.create_all()
