b7b0b67c4098079be6ff06411ffee814f9d8685c
ismaina <maina.wanjau@gmail.com>
2019-08-03 13:28:29 +0300

    added db configs

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,8 @@
-import os
+import os, sys
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
-import sys
+
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -12,6 +13,21 @@ api = Api(app)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 #print(app.config, file=sys.stderr) #ensure proper config was loaded
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 @api.resource('/users/ping')
 class UsersPing(Resource):
